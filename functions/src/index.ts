98fe4c3adfc2ddde078d4640e18ba8869cc50da8
“nissy-yuki” <nisi20010510@gmail.com>
2023-10-11 23:23:13 +0900

    uiレイヤーを追加

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-import {lineBotApp} from "./lineBotApp";
+import {lineBotApp} from "./ui/lineBotApp";
 
 export const lineBot = functions
   .region("asia-northeast1")
