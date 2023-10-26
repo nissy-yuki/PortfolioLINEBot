31307093bf84c4e8f5428ee52a546a8102372f24
“nissy-yuki” <nisi20010510@gmail.com>
2023-10-26 23:20:13 +0900

    ArticleRepositoryにZennServiceを追加

--- functions/src/infra/repository/ArticleRepositoryImpl.ts
+++ functions/src/infra/repository/ArticleRepositoryImpl.ts
@@ -2,6 +2,7 @@ import {inject, injectable} from "tsyringe";
 import {Article} from "../../domain/dataModel/Article";
 import {ArticleRepository} from "../../domain/repository/ArticleRepository";
 import {QiitaService} from "../service/qiita/QiitaService";
+import {ZennService} from "../service/zenn/ZennService";
 
 @injectable()
 /**
@@ -14,14 +15,18 @@ export class ArticleRepositoryImpl implements ArticleRepository {
    */
   constructor(
     @inject("QiitaService")
-    private readonly service: QiitaService
+    private readonly qiitaService: QiitaService,
+    @inject("ZennService")
+    private readonly zennService: ZennService,
   ) { }
   /**
    * 記事を全件取得する
    * @return {Promise<Article[]>}
    */
   async get(): Promise<Article[]> {
-    const articles = await this.service.getArticles();
+    const qiitaArticles = await this.qiitaService.getArticles();
+    const zennArticles = await this.zennService.getArticles();
+    const articles = qiitaArticles.concat(zennArticles);
     return articles;
   }
 }
