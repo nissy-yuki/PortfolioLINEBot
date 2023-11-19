ac897acefd7efca1d2bed66d5eb2806e53706157
“nissy-yuki” <nisi20010510@gmail.com>
2023-11-19 11:18:47 +0900

    不要になったiconUrlを削除

--- functions/src/resource/social.ts
+++ functions/src/resource/social.ts
@@ -5,9 +5,3 @@ export const socialAccountUrl = {
   zenn: "https://zenn.dev/nisisippi",
 };
 
-export const socialIconUrl = {
-  twitter: "https://gyazo.com/26446d078a0546e3b967fddf5a630624",
-  github: "https://gyazo.com/8a48cc20c8e120c91d1e3bbcc6100dcf",
-  qiita: "https://gyazo.com/7240b2067f55076ac8a8207f5d6f04d3",
-  zenn: "https://gyazo.com/e27a8e4a5e7023d9bee103d4298f8984",
-};
